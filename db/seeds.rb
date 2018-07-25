66f54258208b46cc3a940ede3f108d0b2258977b
Winfield Fan <faefaehk@gmail.com>
2018-07-25 18:37:15 +0800

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'KFC', address: 'Pedder Street', phone_number: '12345591', category: 'french')
+
+Restaurant.create(name: 'McDonalds', address: 'Wellington Street', phone_number: '983455351', category: 'italian')
+
+Restaurant.create(name: 'Popeyes', address: 'Hollywood Road', phone_number: '3594551241', category: 'chinese')
+
+Restaurant.create(name: 'Burger King', address: 'Star Avenue', phone_number: '93835591', category: 'belgian')
+
+Restaurant.create(name: 'Tsui Wah', address: 'Gough Street', phone_number: '693482381', category: 'japanese')
+
+
